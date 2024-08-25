34014adea51ab96b79148c38effc90a20e4d7b6c
RyabicovNikita <RyabicovN1k@yandex.ru>
2024-08-25 23:54:53 +0300

    Revert "fix"
    This reverts commit 6d3a84f1228818a945da447e072cbce26f936317.

--- src/components/ErrorsList/ErrorsList.jsx
+++ src/components/ErrorsList/ErrorsList.jsx
@@ -1,5 +1,14 @@
 import styles from './ErrorsList.module.scss';
 
-export const ErrorsList = ({ message }) => {
-	return <p className={styles.error}>{message}</p>;
+export const ErrorsList = ({ errors }) => {
+	const errorValues = Object.values(errors);
+	return (
+		<ol className={styles['errors-list']}>
+			{errorValues.map((error, index) => (
+				<li key={index} className={styles['errors-list__error']}>
+					{error.message}
+				</li>
+			))}
+		</ol>
+	);
 };
